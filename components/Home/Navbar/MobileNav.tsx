803abea5b11eff50a3ee83af49c31bc842985d77
Jorge Matos <jisapjisap@gmail.com>
2025-03-28 10:52:46 +0100

    Fix styling issues in mobile navigation links for better visibility

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -8,11 +8,11 @@ const MobileNav = () => {
       {/* overlay */}
       <div className='fixed inset-0 transform-all duration-500 z-[10002] bg-black opacity-70 w-full h-screen'>
         {/* navlinks */}
-        <div className='text-white fixed justify-center flex flex-col h-full tranform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-pink-600 space-y-6 z-[1050]'>
+        <div className='text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-pink-600 space-y-6 z-[1050]'>
           {navLinks.map((link) => {
             return (
               <Link key={link.id} href={link.url}>
-                <p>
+                <p className='text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]'>
                   {link.label}
                 </p>
               </Link>
